8c6a293bea622d0089dd02eb212c9ab4aaff17a3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-16 15:12:19 +0530

    Refactor API service registration and streamline weather forecast endpoint in Content API

--- src/ShynvTech.Content.Api/Program.cs
+++ src/ShynvTech.Content.Api/Program.cs
@@ -24,17 +24,19 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
-        .ToArray();
-    return forecast;
+        .ToArray(); return forecast;
 })
 .WithName("GetWeatherForecast");
+
+// Map Aspire default endpoints
+app.MapDefaultEndpoints();
 
 app.Run();
 
