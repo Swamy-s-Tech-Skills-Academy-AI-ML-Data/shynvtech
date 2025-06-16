5097ff89e7c7cf0b2a3817becd91aabc15ec1f7d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-16 15:29:04 +0530

    Enhance App.razor with Tailwind CSS and Font Awesome integration; clean up Program.cs by removing unused HttpClient configuration

--- src/ShynvTech.Web/Program.cs
+++ src/ShynvTech.Web/Program.cs
@@ -1,4 +1,3 @@
-using ShynvTech.Web;
 using ShynvTech.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,27 +11,17 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddOutputCache();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.UseOutputCache();
 
 app.MapRazorComponents<App>()
