43c886e46513454989605b46b346f66d6caeb8aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-18 14:18:11 +0530

    Swamy/18jun work (#10)
    * refactor: update Home.razor layout and add missing using directive in _Imports.razor
    
    * refactor: adjust layout and styling in Home.razor for improved presentation
    
    * REmoved old folder
    
    * refactor: remove NavMenu_New.razor component
    
    * refactor: improve layout and styling in Home.razor for better readability and presentation

--- src/ShynvTech.WebV1/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-global using Microsoft.AspNetCore.Components;
-global using Microsoft.AspNetCore.Components.Authorization;
-global using Microsoft.AspNetCore.Components.Forms;
-global using Microsoft.AspNetCore.Components.Routing;
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.Web.Virtualization;
-global using Microsoft.JSInterop;
-global using System.Net.Http;
-global using System.Net.Http.Json;
