173a7784ee38d4d2ae4afbc6754f2c5ce723e2f1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-25 13:13:30 +0530

    Swamy/24jun working on magazine api (#27)
    * Changes
    
    * Update Magazines.razor

--- src/ShyvnTech.Web/Program.cs
+++ src/ShyvnTech.Web/Program.cs
@@ -1,10 +1,24 @@
+using ShyvnTech.ServiceDefaults;
 using ShyvnTech.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add service defaults & Aspire components
+builder.AddServiceDefaults();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add HttpClient for API calls with service discovery
+builder.Services.AddHttpClient("MagazineApi", client =>
+{
+    // This will be resolved by Aspire service discovery
+    client.BaseAddress = new Uri("https://magazine-api/");
+});
+
+// Add default HttpClient
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -24,5 +38,7 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapDefaultEndpoints();
 
 app.Run();
